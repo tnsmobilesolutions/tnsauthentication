b6ab9434389cded237f5b7847ea104e8e5ff4235
Amrutanshu <96327371+Amrutanshu-Chunu@users.noreply.github.com>
2022-10-16 14:01:42 +0530

    uid added @model

--- lib/user_model.dart
+++ lib/user_model.dart
@@ -6,6 +6,7 @@ class userModel {
   String? password;
   String? phoneNumber;
   String? userId;
+  string? uid;
 
   userModel({
     this.name,
@@ -13,6 +14,7 @@ class userModel {
     this.password,
     this.phoneNumber,
     this.userId,
+    this.uid,
   });
 
   userModel copyWith({
@@ -21,6 +23,7 @@ class userModel {
     String? password,
     String? phoneNumber,
     String? userId,
+    string? uid,
   }) {
     return userModel(
       name: name ?? this.name,
@@ -28,28 +31,32 @@ class userModel {
       password: password ?? this.password,
       phoneNumber: phoneNumber ?? this.phoneNumber,
       userId: userId ?? this.userId,
+      uid: uid ?? this.uid,
     );
   }
 
   Map<String, dynamic> toMap() {
     final result = <String, dynamic>{};
-
-    if (name != null) {
+  
+    if(name != null){
       result.addAll({'name': name});
     }
-    if (email != null) {
+    if(email != null){
       result.addAll({'email': email});
     }
-    if (password != null) {
+    if(password != null){
       result.addAll({'password': password});
     }
-    if (phoneNumber != null) {
+    if(phoneNumber != null){
       result.addAll({'phoneNumber': phoneNumber});
     }
-    if (userId != null) {
+    if(userId != null){
       result.addAll({'userId': userId});
     }
-
+    if(uid != null){
+      result.addAll({'uid': uid!.toMap()});
+    }
+  
     return result;
   }
 
@@ -60,6 +67,7 @@ class userModel {
       password: map['password'],
       phoneNumber: map['phoneNumber'],
       userId: map['userId'],
+      uid: map['uid'] != null ? string.fromMap(map['uid']) : null,
     );
   }
 
@@ -70,27 +78,29 @@ class userModel {
 
   @override
   String toString() {
-    return 'userModel(name: $name, email: $email, password: $password, phoneNumber: $phoneNumber, userId: $userId)';
+    return 'userModel(name: $name, email: $email, password: $password, phoneNumber: $phoneNumber, userId: $userId, uid: $uid)';
   }
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-
+  
     return other is userModel &&
-        other.name == name &&
-        other.email == email &&
-        other.password == password &&
-        other.phoneNumber == phoneNumber &&
-        other.userId == userId;
+      other.name == name &&
+      other.email == email &&
+      other.password == password &&
+      other.phoneNumber == phoneNumber &&
+      other.userId == userId &&
+      other.uid == uid;
   }
 
   @override
   int get hashCode {
     return name.hashCode ^
-        email.hashCode ^
-        password.hashCode ^
-        phoneNumber.hashCode ^
-        userId.hashCode;
+      email.hashCode ^
+      password.hashCode ^
+      phoneNumber.hashCode ^
+      userId.hashCode ^
+      uid.hashCode;
   }
 }
